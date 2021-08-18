d6c6d2d4c9772c3c500fd839a96f75940b2875c4
Julia <72216677+0Julia0@users.noreply.github.com>
2021-08-18 15:26:20 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -75,12 +75,13 @@ app.use(auth);
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден.' });
-});
 
 app.use(errorLogger);
 app.use(errors());
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден.' });
+});
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode).send({ message: err.message });
