39c4fa073dad9d4fcc8478a09d665cbf9a1ddeac
Julia <72216677+0Julia0@users.noreply.github.com>
2021-08-28 17:10:49 +0300

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -101,7 +101,10 @@ const createUser = (req, res, next) => {
           email,
           password: hash,
         }))
-        .then((user) => res.status(201).send(user));
+        .then((user) => res.status(201).send({
+          _id: user._id,
+          email: user.email,
+        }));
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
